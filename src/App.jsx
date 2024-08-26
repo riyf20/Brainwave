7fc946d41f0fd413e0a249be4fbaf6e76298ceea
riyf20 <rfard13@gmail.com>
2024-08-26 17:39:05 -0400

    update_2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from './components/Hero';
 import Header from './components/Header';
 import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
+import Services from './components/Services';
+import Pricing from './components/Pricing';
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
       <Hero />
       <Benefits />
       <Collaboration />
+      <Services />
+      <Pricing />
     </div>
     
     <ButtonGradient />
